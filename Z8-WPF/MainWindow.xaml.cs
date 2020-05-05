7b7a51481f0f313a9261ad79f3e0ff1aaf106e06
Pan Ktos <lambda19935@gmail.com>
2020-05-05 09:05:51 +0200

    Dodaj pliki projektów.

---
+++ Z8-WPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z8_WPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        int allAnswersCount = 0;
+        Dictionary<string, int> answers = new Dictionary<string, int>()
+        {
+            { "A", 0 },
+            { "B", 0 },
+            { "C", 0 },
+            { "D", 0 }
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            QuestionTextBlock.Text = "Wolisz A, B, C czy D?";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var choice = button.Content.ToString();
+
+            answers[choice]++;
+            allAnswersCount++;
+
+            allAnswers.Text = allAnswersCount.ToString();
+            RedrawGraph();
+
+
+        }
+        private void RedrawGraph()
+        {
+            var maxHeight = Canvas.ActualHeight;
+
+            RA.Height = maxHeight * (answers["A"] / (double)allAnswersCount);
+            RB.Height = maxHeight * (answers["B"] / (double)allAnswersCount);
+            RC.Height = maxHeight * (answers["C"] / (double)allAnswersCount);
+            RD.Height = maxHeight * (answers["D"] / (double)allAnswersCount);
+
+            Stat2.Text = answers.Max(x => x.Value).ToString();
+        }
+    }
+}
